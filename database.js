c143aa3512b4ac2c4ef4a604774d5d40fd5d0d3d
Raga Subekti <rfilmriot@gmail.com>
2019-05-02 16:34:22 +0700

    Add Password Field

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const { DB_HOST, DB_PASSWORD, DB_COLLECTION_NAME } = process.env;
 
-mongoose.connect(`mongodb://${DB_HOST}/${DB_COLLECTION_NAME}`, {
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PASSWORD}/${DB_COLLECTION_NAME}`, {
   useNewUrlParser: true
 });
 
