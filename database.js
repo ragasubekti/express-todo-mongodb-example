be9aa1695161a8d7457013616a0e315030de71b7
Raga Subekti <rfilmriot@gmail.com>
2019-05-02 16:39:50 +0700

    ADD DB USERR

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const { DB_HOST, DB_PASSWORD, DB_COLLECTION_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_COLLECTION_NAME } = process.env;
 
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PASSWORD}/${DB_COLLECTION_NAME}`, {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_COLLECTION_NAME}`,
+  {
+    useNewUrlParser: true
+  }
+);
 
 module.exports = mongoose;
